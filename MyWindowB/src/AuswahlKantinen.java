1bc726928c470a69667fdd98f688ae8be353d5ec
streetfive <58091801+streetfive@users.noreply.github.com>
2020-03-04 19:27:53 +0100

    Stand 04.03 Peter
     Peter

--- MyWindowB/src/AuswahlKantinen.java
+++ MyWindowB/src/AuswahlKantinen.java
@@ -2,17 +2,20 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Font;
 
 public class AuswahlKantinen {
 
-	private JFrame frame;
+	JFrame frame;
 	private JTextField textUhrzeit;
 	private JTextField text_Datum;
 
@@ -37,87 +40,143 @@ public class AuswahlKantinen {
 	 */
 	public AuswahlKantinen() {
 		initialize();
-	}
 
+		ImageIcon icon = new ImageIcon("src/images/coba_icon.png");
+		frame.setIconImage(icon.getImage());
+		
+		createMenu();
+
+	}
+	//Menuebar erstellen
+
+		public void createMenu() {
+	//Menü -> JmenBar -> -> Jmenu -> JMenuItems
+
+			JMenuBar jMenuBar = new JMenuBar();
+
+			JMenu fileMenu = new JMenu("Programm");
+			JMenu editMenu = new JMenu("Übersicht");
+			JMenu feedbackMenu = new JMenu("Feedback");
+			JMenu sourceMenu = new JMenu("Hilfe");
+
+	//Hinzufügen zur menübar		
+
+			jMenuBar.add(fileMenu);
+			jMenuBar.add(editMenu);
+			jMenuBar.add(feedbackMenu);
+			jMenuBar.add(sourceMenu);
+			
+
+	//Erstellen eines Items Untermenü mit Text und Bild
+
+			JMenuItem exitItem = new JMenuItem("Exit"); // new ImageIcon("scr/images/..png"));
+			JMenuItem editItem = new JMenuItem("Meine Auswahl "); // new ImageIcon("scr/images/..png"));
+			JMenuItem feedbackItem = new JMenuItem("Feedback"); // new ImageIcon("scr/images/..png"));
+			JMenuItem sourceItem = new JMenuItem("Hotline"); // new ImageIcon("scr/images/..png"));
+			
+			//Hinzufügen zur Menübar 	
+
+			fileMenu.add(exitItem);
+			sourceMenu.add(editItem);
+			feedbackMenu.add(editItem);
+			editMenu.add(editItem);
+		
+
+	//Hinzufügen zum Frame Fenster
+
+			frame.setJMenuBar(jMenuBar);
+		
+			
+			
+			
+			
+			
+			
+		}
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		JFrame.setDefaultLookAndFeelDecorated(true); // Look and Feel
 		frame = new JFrame();
 		frame.setBounds(100, 100, 500, 350);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Meet & Eat Planer");
-		lblNewLabel.setBounds(20, 11, 120, 14);
-		
+		lblNewLabel.setBounds(275, 20, 160, 14);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		JRadioButton rdbtnFacetoFace = new JRadioButton("Face to Face");
 		rdbtnFacetoFace.setBounds(20, 70, 109, 23);
 		frame.getContentPane().add(rdbtnFacetoFace);
-		
+
 		JRadioButton rdbtnviererTisch = new JRadioButton("4er-Tisch");
 		rdbtnviererTisch.setBounds(20, 90, 109, 23);
 		frame.getContentPane().add(rdbtnviererTisch);
-		
+
 		JRadioButton rdbtnLastMinute = new JRadioButton("Lastminute");
 		rdbtnLastMinute.setBounds(20, 110, 109, 23);
 		frame.getContentPane().add(rdbtnLastMinute);
-		
+
 		JComboBox auswahlDerKantine = new JComboBox();
+		auswahlDerKantine.addItem("DLZ"); 
+	      auswahlDerKantine.addItem("PLAZA"); 
+	      auswahlDerKantine.addItem("CIELO"); 
+	      auswahlDerKantine.addItem("ESTRELLA");
+	      auswahlDerKantine.addItem("GALLILEO");
+	      auswahlDerKantine.addItem("VITREUM");
+	      
 		auswahlDerKantine.setBounds(20, 181, 109, 30);
 		frame.getContentPane().add(auswahlDerKantine);
-		
-		
+
 		JLabel lblVarianten = new JLabel("Varianten");
 		lblVarianten.setBounds(20, 53, 60, 14);
 		frame.getContentPane().add(lblVarianten);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(165, 181, 157, 30);
 		frame.getContentPane().add(panel);
-		
+
 		text_Datum = new JTextField();
 		panel.add(text_Datum);
 		text_Datum.setColumns(6);
-		
+
 		textUhrzeit = new JTextField();
 		panel.add(textUhrzeit);
 		textUhrzeit.setColumns(5);
-	
-		
+
 		JLabel lbl_Kantine = new JLabel("Auswahl der Kantine");
-		lbl_Kantine.setBounds(20, 156, 109, 14);
+		lbl_Kantine.setBounds(20, 156, 120, 14);
 		frame.getContentPane().add(lbl_Kantine);
-		
+
 		JLabel lbl_Datum = new JLabel("Datum");
 		lbl_Datum.setBounds(174, 156, 46, 14);
 		frame.getContentPane().add(lbl_Datum);
-		
+
 		JLabel lbl_Uhrzeit = new JLabel("Uhrzeit");
 		lbl_Uhrzeit.setBounds(262, 156, 46, 14);
 		frame.getContentPane().add(lbl_Uhrzeit);
-		
+
 		JButton btn_Bestaetigen = new JButton("Best\u00E4tigen");
 		btn_Bestaetigen.setBounds(20, 254, 95, 23);
 		frame.getContentPane().add(btn_Bestaetigen);
-		
+
 		JButton btn_Zurueck = new JButton("Zur\u00FCck");
 		btn_Zurueck.setBounds(119, 254, 89, 23);
 		frame.getContentPane().add(btn_Zurueck);
-		
+
 		JButton btn_abbrechen = new JButton("Abbrechen");
 		btn_abbrechen.setBounds(219, 254, 97, 23);
 		frame.getContentPane().add(btn_abbrechen);
-		
-		JRadioButton rdbtnRichtlinien = new JRadioButton("Ich habe die Bedingungen gelesen und erkl\u00E4re mich einverstanden.");
+
+		JRadioButton rdbtnRichtlinien = new JRadioButton(
+				"Ich habe die Bedingungen gelesen und erkl\u00E4re mich damit einverstanden.");
 		rdbtnRichtlinien.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		rdbtnRichtlinien.setBounds(20, 224, 332, 23);
 		frame.getContentPane().add(rdbtnRichtlinien);
-		
-		
-		
-		
+
 	}
 }
--- MyWindowB/src/Benutzer.java
+++ MyWindowB/src/Benutzer.java
@@ -1,8 +1,10 @@
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+
 
 import backend.CostumerListener;
 
@@ -19,14 +21,14 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-public class Benutzer implements ActionListener{
+public class Benutzer implements ActionListener {
 
 	private JFrame frame;
 	private JTextField txtVorname;
 	private JTextField txtName;
 	private JTextField txtEmail;
-	private JTextField txtNickname;
-	private String NewLunchPartner;
+	private JTextField txtCluster_Cell;
+	String path;
 
 	/**
 	 * Launch the application.
@@ -34,17 +36,16 @@ public class Benutzer implements ActionListener{
 	public static void main(String[] args) {
 //		BenutzerDaten baseB = new BenutzerDaten();     //Database
 //		String [] benutzer = baseB.readAllBenutzer();
-				
-	
+
+//Datei auslesen aus txt. Datei
+
 		LunchPartner newLuncher;
-		
-		
+
 		{
-			
-				
-			
-		};
-		
+
+		}
+		;
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -63,70 +64,74 @@ public class Benutzer implements ActionListener{
 //Konstruktor 
 	public Benutzer() {
 		initialize();
+		
 //Menü aufrufen		
-	createMenu();	
-		
-		
+//		createMenu();
+
+//Image Icon Windowsbar
+
+		ImageIcon icon = new ImageIcon("src/images/coba_icon.png");
+		frame.setIconImage(icon.getImage());
+
 	}
 
-	
 //Menuebar erstellen
-	
+
 	public void createMenu() {
 //Menü -> JmenBar -> -> Jmenu -> JMenuItems
-		
-	JMenuBar jMenuBar = new JMenuBar();
-	
-	JMenu fileMenu = new JMenu("File");
-	JMenu editMenu = new JMenu("Edit");
-	JMenu sourceMenu = new JMenu("Source");
-	
-//Hinzufügen zur menübar		
-	
-	jMenuBar.add(fileMenu);	
-	jMenuBar.add(editMenu);	
-	jMenuBar.add(sourceMenu);	
-	
-//Erstellen eines Items Untermenü mit Text und Bild
-	
-	JMenuItem exitItem = new JMenuItem("Exit", new ImageIcon("src/images/exit.png"));
-//Listener erstellen aus der Klasse CostumerL
-	
-	exitItem.addActionListener(new CostumerListener() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub Hier kommt die Interation mit dem Exit rein
-			System.exit(0);
-		}
-	});
-	
 
-	
+		JMenuBar jMenuBar = new JMenuBar();
+
+		JMenu fileMenu = new JMenu("Programm");
+		JMenu editMenu = new JMenu("Übersicht");
+		JMenu sourceMenu = new JMenu("Hilfe");
+
+//Hinzufügen zur menübar		
+
+		jMenuBar.add(fileMenu);
+		jMenuBar.add(editMenu);
+		jMenuBar.add(sourceMenu);
+
+//Erstellen eines Items Untermenü mit Text und Bild
+
+		JMenuItem exitItem = new JMenuItem("Exit"); // new ImageIcon("scr/images/..png"));
+		JMenuItem editItem = new JMenuItem("Auswahl "); // new ImageIcon("scr/images/..png"));
+
+//	Listener erstellen aus der Klasse CostumerListener
+
+		exitItem.addActionListener(new CostumerListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub Hier kommt die Interation mit dem Exit rein
+				System.exit(0);
+			}
+
+		});
+
 //Hinzufügen zur Menübar 	
-	
-	fileMenu.add(exitItem);
-	
+
+		fileMenu.add(exitItem);
+		editMenu.add(editItem);
+
 //Hinzufügen zum Frame Fenster
-	
-	frame.setJMenuBar(jMenuBar);
-		
+
+		frame.setJMenuBar(jMenuBar);
+
 	}
-	
-	
+
 //Dialogfenster	erstellen für Meldungen an den Benutzer
-	
+
 	private void createMessage(String message) {
 		JOptionPane.showMessageDialog(frame, message);
 	}
-	
-	
-	
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
 // Layout setBounds = x-Position, y-Position, breite und höhe
 	private void initialize() {
+		JFrame.setDefaultLookAndFeelDecorated (true);		//Look and Feel
 		frame = new JFrame();
 		frame.setBounds(100, 100, 500, 350);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Fenster wird richtig nach schließen geschlossen
@@ -149,17 +154,17 @@ public class Benutzer implements ActionListener{
 // Textfelder aus der Benutzereingabe 
 		
 		txtVorname = new JTextField();
-		txtVorname.setBounds(80, 81, 171, 20);
+		txtVorname.setBounds(90, 81, 171, 20);
 		frame.getContentPane().add(txtVorname);
 		txtVorname.setColumns(10);
 		
 		txtName = new JTextField();
-		txtName.setBounds(80, 112, 171, 20);
+		txtName.setBounds(90, 112, 171, 20);
 		frame.getContentPane().add(txtName);
 		txtName.setColumns(10);
 		
 		txtEmail = new JTextField();
-		txtEmail.setBounds(80, 143, 171, 20);
+		txtEmail.setBounds(90, 143, 171, 20);
 		frame.getContentPane().add(txtEmail);
 		txtEmail.setColumns(10);
 		
@@ -167,95 +172,124 @@ public class Benutzer implements ActionListener{
 
 		
 		JLabel lblVorname = new JLabel("Vorname");
-		lblVorname.setBounds(18, 84, 46, 14);
+		lblVorname.setBounds(18, 84, 60, 14);
 		frame.getContentPane().add(lblVorname);
 		
 		JLabel lblName = new JLabel("Name");
 		lblName.setBounds(18, 115, 46, 14);
 		frame.getContentPane().add(lblName);
 		
-		JLabel lblemail = new JLabel("EMail");
+		JLabel lblemail = new JLabel("E-Mail");
 		lblemail.setBounds(18, 146, 46, 14);
 		frame.getContentPane().add(lblemail);
 
 		
-		txtNickname = new JTextField();
-		txtNickname.setBounds(80, 174, 171, 20);
-		frame.getContentPane().add(txtNickname);
-		txtNickname.setColumns(10);
+		txtCluster_Cell = new JTextField();
+		txtCluster_Cell.setBounds(90, 174, 171, 20);
+		frame.getContentPane().add(txtCluster_Cell);
+		txtCluster_Cell.setColumns(10);
 		
 
 		
-		JLabel lblNickName = new JLabel("Nickname");
-		lblNickName.setBounds(18, 177, 46, 14);
+		JLabel lblNickName = new JLabel("Cluster Cell");
+		lblNickName.setBounds(18, 177, 58, 14);
 		frame.getContentPane().add(lblNickName);
 		
-		JLabel lblStammdaten = new JLabel("Stammdaten");
+		JLabel lblStammdaten = new JLabel("Anmeldedaten");
 		lblStammdaten.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblStammdaten.setBounds(18, 11, 115, 14);
 		frame.getContentPane().add(lblStammdaten);
 		
+		JButton btnAbbrechen = new JButton("Abbrechen");
+		btnAbbrechen.setBounds(195, 219, 100, 23);
+		frame.getContentPane().add(btnAbbrechen);
+		
 		JButton btnBestaetigen = new JButton("Bestaetigen");
+		btnBestaetigen.setBounds(78, 219, 105, 23);
+		frame.getContentPane().add(btnBestaetigen);
+		
+		
 		btnBestaetigen.addActionListener(new ActionListener() {
 			
 //Nach Eingabe der Benutzerdaten soll ein neuer Datensatz angelegt werden			
 			public void actionPerformed(ActionEvent e) {
 				
 				String vorname = txtVorname.getText();
-				String name = txtName.getSelectedText();
-				String email = txtEmail.getSelectedText();
-				String nickname = txtNickname.getSelectedText();
-				LunchPartner einNeuerLunchPartner = new LunchPartner(txtVorname, txtName, txtEmail, txtNickname);
-			}
-		});
-		btnBestaetigen.setBounds(71, 219, 83, 23);
-		frame.getContentPane().add(btnBestaetigen);
+				String name = txtName.getText();
+				String email = txtEmail.getText();
+				String nickname = txtCluster_Cell.getText();
+				LunchPartner einNeuerLunchPartner = new LunchPartner(txtVorname, txtName, txtEmail, txtCluster_Cell);
+				System.out.println("Ein neuer Lunch Partner " + einNeuerLunchPartner );
+	//			if(e.getSource()==btnBestaetigen path == (open C:\\Programme (x86)\\Hobby\\Firefox.exe");
+				{
+				
+				
+				
+	//		
+				
+				
+			}{
+				
+				
+			
+				;
+				btnAbbrechen.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						
+						// TODO Auto-generated method stub
+						if(e.getSource() == btnAbbrechen)
+						JOptionPane.showMessageDialog(null, "Möchten Sie das Programm wirklich beenden?");
+						System.exit(0);
+//						System.out.println("Funktioniert");	
+					}
+				});             //Prüft auf ein Event	
+				
+					
+					
+					
+					
+				}
+				
+				
+			}	
 		
-		JButton btnAbbrechen = new JButton("Abbrechen");
-		btnAbbrechen.addActionListener(this);             //Prüft auf ein Event
-		btnAbbrechen.setBounds(170, 219, 85, 23);
-		frame.getContentPane().add(btnAbbrechen);
+		});
+	
+		
+	
 	}
 
-	
 //Methoden von BenutzerDaten
-	
+
 	public String[] readAllBenutzer() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public void addBenutzer(String name) {
 		// TODO Auto-generated method stub
-	
-	
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
-	
-	
+
 //Event Handling von der implements der Klasse
-	
+
 //	@Override
 //	public void actionPerformed(ActionEvent e) {             // Action Event durch Implements hinzugefügt
 //		System.out.println("Abbrechen");
-		
+
 //	if (e.getSource() == btnBestaetigen){    // Was soll passieren, wenn die Eingabe von den einzelnen Benutzerfelder erfolgt ist
 //		String vornameString = txtVorname.getText();
 //		char [] chars = txtVorname.get;	
 //		String txtVornameString = new String(chars); //Char wieder in String umwandeln
-		
-		
+
 //		if (e.getSource() == btnAbbrechen){
-	
-	}
 
-
-
-	
+}
